a8bd20b06b7569f32fae8be7846ec68bb53e87b9
Geoff Day <geoffday67@gmail.com>
2018-04-09 12:35:35 +0200

    Solved problem 601

--- euler_601/main.py
+++ euler_601/main.py
@@ -28,7 +28,7 @@ def powers(n):
     return result
 
 
-def lcm(n1, n2):
+def singlelcm(n1, n2):
     p3 = {}
     p1 = powers(n1)
     p2 = powers(n2)
@@ -46,9 +46,33 @@ def lcm(n1, n2):
     return result
 
 
-print(powers(100))
+def lcm(*args):
+    if len(args) == 1:
+        return args[0]
+
+    result = singlelcm(args[0], args[1])
+    for arg in args:
+        result = singlelcm (result, arg)
+    return result
+
+
+def P(s, N):
+    l1 = lcm(*range(2, s + 1))
+    l2 = lcm(*range(2, s + 2))
+
+    return ((N-2) // l1) - ((N-2) // l2)
+
+# print(powers(100))
 
 # l = 1
 # for n in range(1, 20):
 #    l = lcm (l, n)
 # print (l)
+
+total = 0
+for i in range(2, 32):
+    total += P(i, 4**i)
+
+total += 1
+
+print (total)
